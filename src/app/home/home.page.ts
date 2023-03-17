5b450e4e61181b0e0a3816f9fc5de1ab52d34721
0DanielR <102000459+0DanielR@users.noreply.github.com>
2023-03-17 12:05:31 -0700

    Primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { type } from 'os';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  public products = [
+    {
+      name:"CocaCola",
+      price:20,
+      type:"Abarrotes"
+    },
+    {
+      name:"Jabón Zote",
+      price:30,
+      type:"Limpieza"
+    },
+    {
+      name:"Croquetas 50kg",
+      price:400,
+      type:"Mascotas"
+    },
+    {
+      name:"Zucaritas 500g",
+      price:100,
+      type:"Abarrotes"
+    }
+  ];//Arreglo de objetos
   constructor() {}
 
+  getColor(type: string): string{
+    let color="";
+    switch(type){
+      case "Abarrotes":
+        color="primary";
+        break;
+      case "Limpieza":
+        color="success";
+        break;
+      case "Mascotas":
+        color="warning";
+        break;
+      default:
+        break;
+    }
+    return color;
+  }
+
 }
+
+
+
+
